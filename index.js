12380343333d41e6f9ffab077dddb4ca52de552e
German Arcila <germanarcila@gmail.com>
2020-01-29 21:04:06 -0500

    separated data and started on prompt generation

--- index.js
+++ index.js
@@ -1,70 +1,42 @@
 const button = document.querySelector('.btn');
-let passwordLength;
-let passwordCharacters;
 
-const alphabet = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-];
-const specialCharacters = [
-  '!',
-  '"',
-  '#',
-  '$',
-  '%',
-  '&',
-  '(',
-  ')',
-  '*',
-  '+',
-  ',',
-  '-',
-  '.',
-  '/',
-  ':',
-  ';',
-  '<',
-  '=',
-  '>',
-  '?',
-  '@',
-  '[',
-  '\\',
-  ']',
-  '^',
-  '_',
-  '`',
-  '{',
-  '|',
-  '}',
-  '~',
-  "'",
-];
+const options = {
+  passwordLength: null,
+  specialCharacters: null,
+  numbers: null,
+  lowercase: null,
+  uppercase: null,
+};
 
-const generatePassword = () => {};
+const usersCharacters = [];
+
+const chooseCharacter = () => {
+  return Math.floor(Math.random() * usersCharacters.length);
+};
+
+const generatePassword = () => {
+  options.passwordLength = prompt(
+    'choose a length of at least 8 characters and no more than 128 characters'
+  );
+
+  //TODO: need to create check to confirm if a number was provided
+  //TODO: need to check if lenght is 8 or greater
+  //TODO: need to check if length is 128 or less
+  //TODO: need to provide messages for each case above.  If it does not match, go back
+
+  options.specialCharacters = confirm(
+    'Click OK to confirm including special characters'
+  );
+  options.nummbers = confirm(
+    'Click OK to confirm including numeric characters'
+  );
+
+  options.lowercase = confirm(
+    'Click OK to confirm including lowercase characters'
+  );
+  options.uppercase = confirm(
+    'Click OK to confirm including uppercase characters'
+  );
+};
 
 button.addEventListener('click', generatePassword);
